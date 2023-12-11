9a17eb66cd224dd6c93061a3bc716129fd62fc68
moisesfilhosenai <moises.filho@docente.senai.br>
2023-12-11 18:19:25 -0300

    Atualizando

--- app/src/main/java/br/com/senai/calculoimc/IMC.java
+++ app/src/main/java/br/com/senai/calculoimc/IMC.java
@@ -5,6 +5,7 @@ public class IMC {
     private float altura;
     private float resultado;
     private String classificacao;
+    private int imagemId;
 
     public IMC(float peso, float altura) {
         this.peso = peso;
@@ -23,22 +24,34 @@ public class IMC {
         return classificacao;
     }
 
+    public int getImagemId() {
+        return imagemId;
+    }
+
     public void classificar() {
         if (this.resultado < 18.5) {
             this.classificacao = "Baixo peso";
+            this.imagemId = R.drawable.magreza;
         } else if (this.resultado >= 18.5 &&
                 this.resultado <= 24.9) {
             this.classificacao = "Peso normal";
+            this.imagemId = R.drawable.normal;
         } else if (this.resultado >= 25 &&
                 this.resultado <= 29.9) {
             this.classificacao = "Sobrepeso";
+            this.imagemId = R.drawable.obeso;
         } else if (this.resultado >= 30 &&
                 this.resultado <= 34.9) {
             this.classificacao = "Obesidade grau I";
+            this.imagemId = R.drawable.obesidade2;
         } else if (this.resultado >= 35 &&
                 this.resultado <= 39.9) {
             this.classificacao = "Obesidade grau II";
-        } else if (this.resultado > 40)
+            this.imagemId = R.drawable.obesidade2;
+        } else if (this.resultado > 40) {
             this.classificacao = "Obesidade grau III";
+            this.imagemId = R.drawable.obesidade2;
+        }
     }
+
 }
--- app/src/main/java/br/com/senai/calculoimc/MainActivity.java
+++ app/src/main/java/br/com/senai/calculoimc/MainActivity.java
@@ -3,12 +3,15 @@ package br.com.senai.calculoimc;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText etAltura;
     private Button btCalcular;
     private IMC imc;
+    private ImageView imageV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         etPeso = findViewById(R.id.etPeso);
         etAltura = findViewById(R.id.etAltura);
         btCalcular = findViewById(R.id.btCalcular);
+        imageV = findViewById(R.id.imageV);
 
         btCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,10 +48,12 @@ public class MainActivity extends AppCompatActivity {
                     String inputAltura = etAltura.getText().toString();
                     float altura = Float.parseFloat(inputAltura);
 
+                    InputMethodManager imm = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
                     MainActivity.this.calcularIMC(peso, altura);
                 }
-
-                
             }
         });
     }
@@ -56,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
         this.imc.calcular();
         this.imc.classificar();
         this.exibirResultado();
+        this.imageV.setImageResource(this.imc.getImagemId());
+
+
     }
 
     private void exibirResultado() {
